d71e1d63161117298e744e76633f43b4a4a606eb
dfilipkowski <damian.filipkowski@idego.io>
2021-04-12 12:26:30 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e1a9949b2815a2746053.js"
+    "url": "webpack-runtime-1e6437ea6f5029b5dfda.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a408facbd6fba9035e2a519912b4b38e"
+    "revision": "6dd2aa542fba83a2f726815e1d8a8b3e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "5751ee8c89eb4db9e28aca825593c742"
+    "revision": "61030f7ab20d13c8871ab6ac57368ce3"
   },
   {
     "url": "polyfill-57fa76c8ad53bd44c5a1.js"
